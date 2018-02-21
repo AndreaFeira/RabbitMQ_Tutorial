473f275f098c12e9d2c71a1c3112c62dd5c3c11c
afeira <afeira@topcon.com>
2018-02-21 14:57:01 +0100

    First step. Hello world.

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Consumer
+    {
+        public static void Main()
+        {
+            try
+            {
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    //The queue must be declared both in Produced and Consumer cause we might start the consumer before the publisher,
+                    //This make sure the queue exists before try to consume messages from it.
+                   channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        byte[] body = ea.Body;
+                        string message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "hello",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/Producer.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Producer
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+
+                //The connection abstracts the socket connection, and takes care of protocol version negotiation and authentication and so on...
+                using (IConnection connection = factory.CreateConnection())
+                {
+                    if (!connection.IsOpen) throw new ApplicationException("Connection is close.");
+
+                    using (IModel channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "hello_queue",
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+                        Console.WriteLine("Press [enter] to proceed.");
+                        Console.ReadLine();
+
+                        string message = "Hello World!";
+
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+
+                        Console.ReadLine();
+
+                        channel.BasicPublish(exchange: "",
+                            routingKey: "hello",
+                            basicProperties: null,
+                            body: body);
+
+                        Console.WriteLine("Press [enter] to exit.");
+                        Console.ReadLine();
+                    }
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
